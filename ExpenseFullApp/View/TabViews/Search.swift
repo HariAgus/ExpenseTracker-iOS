dd5af8888f77a69dd0907aa89f0c4db035c35d38
HariAgus <haryagus97@gmail.com>
2024-01-02 14:04:19 +0700

    Create Search View and Setting View

--- ExpenseFullApp/View/TabViews/Search.swift
+++ ExpenseFullApp/View/TabViews/Search.swift
@@ -6,10 +6,50 @@
 //
 
 import SwiftUI
+import Combine
 
 struct Search: View {
+    
+    // View Properties
+    @State private var searchText: String = ""
+    @State private var filterText: String = ""
+    let searchPublisher = PassthroughSubject<String, Never>()
+    
     var body: some View {
-        Text("Hari Agus")
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVStack(spacing: 12) {
+                    
+                }
+            }
+            .overlay {
+                VStack {
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 42.0, height: 42.0)
+                        .padding(.bottom, 12)
+                    
+                    Text("Search Transactions")
+                        .font(.title3)
+                        .bold()
+                }
+                .opacity(filterText.isEmpty ? 1 : 0)
+            }
+            .onChange(of: searchText, perform: { newValue in
+                if newValue.isEmpty {
+                    filterText = ""
+                }
+                
+                searchPublisher.send(newValue)
+            })
+            .onReceive(searchPublisher.debounce(for: .seconds(0.3), scheduler: DispatchQueue.main), perform: { text in
+                filterText = text
+            })
+            .searchable(text: $searchText)
+            .navigationTitle("Search")
+            .background(.gray.opacity(0.15))
+            
+        }
     }
 }
 
